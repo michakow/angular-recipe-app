998705a79cb5cc2bc2af4c8546bca25eff9d07ae
Michal Kowalczyk <mickowalczyk6@gmail.com>
2022-03-26 21:05:16 +0100

    add subheading to header and binding input data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  appName: string = 'Przepiśnik'
 
   constructor() {}
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,10 @@ import { MainComponent } from './components/main/main.component';
 import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
 import { DetailsComponent } from './components/details/details.component';
 import { RecipeTileComponent } from './components/recipe-tile/recipe-tile.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, UppercaseFirstLetterPipe, HeaderComponent, MainComponent, RecipeListComponent, DetailsComponent, RecipeTileComponent],
+  declarations: [AppComponent, UppercaseFirstLetterPipe, HeaderComponent, MainComponent, RecipeListComponent, DetailsComponent, RecipeTileComponent, FooterComponent],
   imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
