37a89243fedb63094faa58a1ba576fd4117c565e
Michal Kowalczyk <mickowalczyk6@gmail.com>
2022-03-26 22:57:21 +0100

    add pipe to transform rating into icons

---
+++ src/app/pipes/transform-rating-to-icons.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TransformRatingToIconsPipe } from './transform-rating-to-icons.pipe';
+
+describe('TransformRatingToIconsPipe', () => {
+  it('create an instance', () => {
+    const pipe = new TransformRatingToIconsPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
