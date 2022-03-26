8b5d12f407f5c0e274acaa1e2b231066fdeb371d
Michal Kowalczyk <mickowalczyk6@gmail.com>
2022-03-27 00:00:28 +0100

    other small changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { UppercaseFirstLetterPipe } from './pipes/uppercase-first-letter.pipe';
 import { HeaderComponent } from './components/header/header.component';
 import { MainComponent } from './components/main/main.component';
@@ -11,9 +11,10 @@ import { RecipeListComponent } from './components/recipe-list/recipe-list.compon
 import { DetailsComponent } from './components/details/details.component';
 import { RecipeTileComponent } from './components/recipe-tile/recipe-tile.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { TransformRatingToIconsPipe } from './pipes/transform-rating-to-icons.pipe';
 
 @NgModule({
-  declarations: [AppComponent, UppercaseFirstLetterPipe, HeaderComponent, MainComponent, RecipeListComponent, DetailsComponent, RecipeTileComponent, FooterComponent],
+  declarations: [AppComponent, UppercaseFirstLetterPipe, HeaderComponent, MainComponent, RecipeListComponent, DetailsComponent, RecipeTileComponent, FooterComponent, TransformRatingToIconsPipe],
   imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
