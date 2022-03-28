b63e7d66c4c370fd29fd286699f973157f5eeda5
Michal Kowalczyk <michal.kowalczyk@ppuc.pl>
2022-03-28 17:52:18 +0200

    add form with basic logic

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
-export class FormComponent implements OnInit {
+export class FormComponent implements OnInit, AfterViewInit {
+  recipeForm!: FormGroup;
+  withoutIng: boolean = true;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit(): void {
+  get ingriedientsFormArray() {
+    return this.recipeForm.controls['ingriedients'] as FormArray;
   }
 
+  get ingriedientsFormGroup() {
+    return this.ingriedientsFormArray.controls as FormGroup[];
+  }
+
+  ngOnInit(): void {
+    this.recipeForm = this.createForm();
+  }
+
+  ngAfterViewInit(): void {}
+
+  addNextIngriedient() {
+    this.ingriedientsFormArray.push(
+      new FormGroup({
+        ingName: this.formBuilder.control('', [Validators.required]),
+        ingValue: this.formBuilder.control('', [Validators.required]),
+      })
+    );
+  }
+
+  removeIngriedient(index: number) {
+    this.ingriedientsFormArray.removeAt(index);
+  }
+
+  onSubmit() {
+    this.recipeForm.markAllAsTouched();
+    if (this.recipeForm.invalid) return;
+    console.log(this.recipeForm.value);
+  }
+
+  private createForm(): FormGroup {
+    const form = (this.recipeForm = this.formBuilder.group({
+      name: this.formBuilder.control('', [Validators.required, Validators.minLength(4)]),
+      description: this.formBuilder.control('', [Validators.required]),
+      ingriedients: this.formBuilder.array([
+        this.formBuilder.group({
+          ingName: this.formBuilder.control('', [Validators.required]),
+          ingValue: this.formBuilder.control('', [Validators.required]),
+        }),
+      ]),
+    }));
+
+    return form;
+  }
 }
--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -2,19 +2,19 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AdminGuard implements CanActivate {
-  constructor(private router: Router){}
+  constructor(private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    if(prompt() === 'admin'){
-      return true
-    }
+    // if(prompt() === 'admin'){
+    //   return true
+    // }
 
-    this.router.navigate(['recipes']);
+    // this.router.navigate(['recipes']);
 
-    return false;
+    // return false;
+    return true;
   }
-
 }
