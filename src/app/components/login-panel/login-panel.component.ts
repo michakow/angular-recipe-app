c0962105ff2723084efcfcb0760c173865eb2576
Michal Kowalczyk <michal.kowalczyk@ppuc.pl>
2022-03-29 15:49:33 +0200

    finish login panel

--- src/app/components/login-panel/login-panel.component.ts
+++ src/app/components/login-panel/login-panel.component.ts
@@ -1,4 +1,7 @@
+import { LocalizedString } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-panel',
@@ -6,7 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-panel.component.scss'],
 })
 export class LoginPanelComponent implements OnInit {
-  constructor() {}
+  loginForm!: FormGroup;
 
-  ngOnInit(): void {}
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.createForm();
+  }
+
+  onSubmit() {
+    this.loginForm.markAllAsTouched();
+    if (this.loginForm.invalid) return;
+    localStorage.setItem('logged', 'true');
+    localStorage.setItem('userName', JSON.stringify(this.loginForm.value.login));
+    this.router.navigate(['recipes']);
+  }
+
+  remindPassword() {
+    console.log('przypomnij haslo');
+  }
+
+  goToRegister() {
+    console.log('zarejestruj');
+  }
+
+  private createForm(): FormGroup {
+    const form = this.formBuilder.group({
+      login: this.formBuilder.control('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$')]),
+      password: this.formBuilder.control('', [Validators.required]),
+    });
+
+    return form;
+  }
 }
