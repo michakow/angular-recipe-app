d8e39a920391cf125a2bbfb094da946f60bcd6a8
Michal Kowalczyk <michal.kowalczyk@ppuc.pl>
2022-04-04 15:14:17 +0200

    fetch only own recipes when user role is author

--- src/app/services/recipe-api.service.ts
+++ src/app/services/recipe-api.service.ts
@@ -1,26 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Recipe } from '../interfaces/recipe';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RecipeApiService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private userService: UserService) {}
 
   getRecipes() {
-    return this.http.get<Recipe[]>('http://localhost:3000/recipes');
+    if (this.userService.getRole() === 'user') return this.http.get<Recipe[]>('http://localhost:3000/recipes');
+    return this.http.get<Recipe[]>(`http://localhost:3000/recipes?author=${this.userService.getName()}`);
   }
 
   getRecipeByID(id: number) {
     return this.http.get<Recipe>(`http://localhost:3000/recipes/${id}`);
   }
 
-  getRecipesWithName(name: string){
-    return this.http.get<Recipe[]>(`http://localhost:3000/recipes?name_like=${name}`);
+  getRecipesWithName(name: string) {
+    return this.http.get<Recipe[]>(`http://localhost:3000/recipes?name_like=${name}&author=${this.userService.getName()}`);
   }
 
-  addRecipe(recipe: Recipe){
-    return this.http.post<Recipe>('http://localhost:3000/recipes', {...recipe});
+  addRecipe(recipe: Recipe) {
+    return this.http.post<Recipe>('http://localhost:3000/recipes', { ...recipe });
   }
 }
